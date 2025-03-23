4b5ca05763536e04874c82b4b460f1454e8e22f7
Luiz só dela <Luizgustavosantos007@Gmail.com>
2025-03-23 03:50:26 -0300

    Update naju.js
    correções de corações na tela

--- naju.js
+++ naju.js
@@ -2,16 +2,23 @@ function createHeart() {
     let heart = document.createElement('div');
     heart.classList.add('heart');
     heart.innerText = '❤️';
-    const randomX = Math.random() * window.innerWidth;
-    const randomY = Math.random() * (window.innerHeight);
+    
+    
+    const randomX = Math.random() * (window.innerWidth - 50); 
+    const randomY = Math.random() * (window.innerHeight - 50); 
+    
     heart.style.left = randomX + 'px';
     heart.style.top = randomY + 'px';
-    heart.style.animationDuration = Math.random() * 5 + 5 + 's';
-    heart.style.fontSize = Math.random() * 20 + 20 + 'px';
+    heart.style.animationDuration = Math.random() * 5 + 5 + 's'; 
+    heart.style.fontSize = Math.random() * 20 + 20 + 'px'; 
+    
     document.body.appendChild(heart);
+
+    
     setTimeout(() => {
         heart.remove();
     }, parseFloat(heart.style.animationDuration) * 1000);  
 }
+
 
 setInterval(createHeart, 300);
