c500c8e191806b64d28c0cf3ca2a0fcade6006a4
Luiz só dela <Luizgustavosantos007@Gmail.com>
2025-03-23 03:53:13 -0300

    Update naju.js
    update para Celular

--- naju.js
+++ naju.js
@@ -1,17 +1,29 @@
+
+const maxHearts = 50; 
+
+
 function createHeart() {
+    
+    const currentHearts = document.querySelectorAll('.heart').length;
+
+   
+    if (currentHearts >= maxHearts) {
+        return;
+    }
+
     let heart = document.createElement('div');
     heart.classList.add('heart');
     heart.innerText = '❤️';
-    
+
     
     const randomX = Math.random() * (window.innerWidth - 50); 
     const randomY = Math.random() * (window.innerHeight - 50); 
-    
+
     heart.style.left = randomX + 'px';
     heart.style.top = randomY + 'px';
     heart.style.animationDuration = Math.random() * 5 + 5 + 's'; 
     heart.style.fontSize = Math.random() * 20 + 20 + 'px'; 
-    
+
     document.body.appendChild(heart);
 
     
